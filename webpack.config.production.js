96a5b034fe934b94663fccc30cee92b7498625a6
minimal <jcm940308@gmail.com>
2022-11-27 16:39:05 +0900

    feature: Config linter, prettier, bundler, typescript

---
+++ webpack.config.production.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    main: "./src/index.tsx",
+    content: "./content/index.ts",
+  },
+  output: {
+    filename: (pathData) => {
+      return pathData.chunk.name === "main" ? "[name].js" : "content/[name].js";
+    },
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: "swc-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.resolve(__dirname, "static/index.html"),
+      chunksSortMode: "none",
+      chunks: ["main"],
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "static/manifest.json",
+          to: "manifest.json",
+        },
+      ],
+    }),
+  ],
+};
