2e602f2d3505ef934c7998675fb90212068fce6c
minimal <jcm940308@gmail.com>
2022-11-27 20:49:03 +0900

    fix: fix to path for copy-webpack-plugin

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -11,6 +11,7 @@ module.exports = {
       patterns: [
         {
           from: "static/*",
+          to: "[name][ext]",
           filter: (filepath) => {
             return !filepath.includes("index.html");
           },
